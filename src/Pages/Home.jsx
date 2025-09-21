5013fe7c1c4091b28d2e59b6f1e0617b36899941
H4RI2000 <hari2000atl@gmail.com>
2025-09-21 17:18:41 +0530

    Fix Home.jsx imports and prepare frontend for Render

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,54 +1,49 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import Feature from '../Components/Feature'
-import About from '../Components/About'
-import Departments from '../Components/Departments'
-import Whyus from '../Components/whyus'
-import Counter from '../Components/Counter'
-import Team from '../Components/Team'
-import ContactForm from '../Components/ContactForm'
-
-
-
-
+import React from "react";
+import Hero from "../Components/Hero";
+import Feature from "../Components/Feature";
+import About from "../Components/About";
+import Departments from "../Components/Departments";
+import WhyUs from "../Components/WhyUs"; // Make sure file is exactly WhyUs.jsx
+import Counter from "../Components/Counter";
+import Team from "../Components/Team";
+import ContactForm from "../Components/ContactForm";
 
 const Home = () => {
-    return (
-        <>
+  return (
+    <>
+      <section>
+        <Hero />
+      </section>
 
-            <section>
-                <Hero />
+      <section>
+        <Feature />
+      </section>
 
-            </section>
-            <section>
-                <Feature />
-            </section>
-            <section>
-                <About />
-            </section>
-            <section>
-                <Whyus />
-            </section>
-            <section>
-                <Counter />
-            </section>
-            <section>
-                <Departments />
-            </section>
-            <section>
-                <Team />
+      <section>
+        <About />
+      </section>
 
-            </section>
-            <section>
-                <ContactForm />
-            </section>
+      <section>
+        <WhyUs />
+      </section>
 
+      <section>
+        <Counter />
+      </section>
 
+      <section>
+        <Departments />
+      </section>
 
+      <section>
+        <Team />
+      </section>
 
+      <section>
+        <ContactForm />
+      </section>
+    </>
+  );
+};
 
-
-        </>
-    )
-}
-export default Home
+export default Home;
